612accb09a39e75ee0a30406567494b87315c883
Stuart Goodwin <sgoodwin@anki.com>
2017-09-13 16:23:07 -0700

    combined ticket gathering with text search algo & result output

--- phrase_search_algo.py
+++ phrase_search_algo.py
@@ -1,17 +1,3 @@
-##phraseget
-
-#SPECIAL NOTE: extracts and prints to screen part of sentnece using keyword
-#Change keyword at bottom of code labled CHANGE KEYWORD
-
-#instructions add texts to directory newcorpus on your desktop
-#run this program-be patient
-#results stored on desktop in output.txt
-
-
-# kept adding these and they work dont ask details
-# NOTE MUST install NLTK
-
-
 from __future__ import division
 import nltk, re, pprint, os
 from nltk import sent_tokenize, word_tokenize
@@ -27,42 +13,90 @@ from nltk.text import Text
 import glob
 import shutil
 import time
+import errno
+import os, os.path
+import requests
+from collections import defaultdict
+from datetime import date, datetime, timedelta
 
 
-# First set the variable path to the directory path.  Use
-# forward slashes (/), even on Windows.  Make sure you
-# leave a trailing / at the end of this variable.
+default_stdout = sys.stdout
 
-# Path for mac OS X will look something like this:
-#text files from zendesk python api text algo script dump to home directory and need to be moved to below path
-path = "/Users/Stuart/newcorpus/"
+startTime = datetime.now()
+
+date_filter = input('Enter desired date period for all tickets after specified date(format: yyyy-mm-dd):')
+
+key_phrase = input('Enter phrase to search for:')
+
+#path = input('Enter path to dump text files:')
+
+search_string = 'type:ticket created>' + date_filter
+
+search_url = 'https://anki.zendesk.com/api/v2/search.json?query=' + search_string
+user = 'sgoodwin@anki.com' + '/token'
+pwd = 'wlekVMUjMxALMZTzZsf1F5liBGjR76FNsCx20tQZ'
 
 
-outfilename = 'newcorpus_condensed_' + str((int(time.time()))) + ".txt"
+#gather ticket list based on date filter input
 
+search_response = requests.get(search_url, auth=(user, pwd))
 
-with open(outfilename, 'wb') as outfile:
-    for filename in glob.glob('/Users/Stuart/newcorpus/*.txt'):
-        if filename == outfilename:
-            # don't want to copy the output into the output
-            continue
-        with open(filename, 'rb') as readfile:
-            shutil.copyfileobj(readfile, outfile)
+search_data = search_response.json()
 
-           
-            
-#superkeywords = 'Cozmo'
+ticket_list = []
 
-text = open(outfilename, "r").read()
-tokens = word_tokenize(text)
-textList = Text(tokens)
-#for word in superkeywords:
-    #textList.concordance(word, 75, sys.maxsize)
-#print(textList)
+print('gathering ticket list...')
+
+while search_url:
+    search_response = requests.get(search_url, auth=(user, pwd))
+
+    search_data = search_response.json()
+  
+
+    for field in search_data['results']:
+        for k,v in field.items():
+            if k == 'id':
+                ticket_list.append(str(v))
+    search_url = search_data['next_page']
+
     
+
+# loop through desired ticket list and output comments as txt file for each ticket
+
+print('gathering fields from ticket list...')
+
+
+for x in ticket_list:
+
+    url = 'https://anki.zendesk.com/api/v2/tickets/' + x + '/comments.json'
+    user = 'sgoodwin@anki.com' + '/token'
+    pwd = 'wlekVMUjMxALMZTzZsf1F5liBGjR76FNsCx20tQZ'
+
+    response = requests.get(url, auth=(user, pwd))
+
+    data = response.json()
+
+    dict_list = []
+
+    for comment in data['comments']:
+        for k,v in comment.items():
+            if k == 'id':
+                comment_id = str(v)
+            if k == 'body':
+                comment = str(v)
+            if k == 'created_at':
+                created_date = str(v)
+
+        dict_list.append(comment)
     
+        
+    with open(os.path.join('/Users/stuart/newcorpus/','ticket-' + str(x) + '.txt'),'w') as outfile:
+        sys.stdout = outfile
+        outfile.write("\n".join(dict_list))   
+
+sys.stdout = default_stdout
+
 def n_concordance_tokenised(text,phrase,left_margin=5,right_margin=5):
-    #concordance replication via https://simplypython.wordpress.com/2014/03/14/saving-output-of-nltk-text-concordance/
     phraseList=phrase.split(' ')
  
     c = nltk.ConcordanceIndex(text.tokens, key = lambda s: s.lower())
@@ -81,100 +115,35 @@ def n_concordance_tokenised(text,phrase,left_margin=5,right_margin=5):
                           
     outputs=[''.join([x+' ' for x in con_sub]) for con_sub in concordance_txt]
     return outputs
- 
-
-results = n_concordance_tokenised(textList, 'Customer Care')
-
-keyword_matches = len(results)
-
-print(str(keyword_matches) + ' ' + 'keyword matches found') 
 
 
+print("searching tickets for phrase...")
 
----------------------
+#file_path = '/Users/stuart/newcorpus/*.txt'   
 
-#stopwordcount
+sys.stdout = open("phrase_matches.txt", "w")
 
-#instructions add texts to directory newcorpus on your desktop
-#run this program-be patient
-#results stored on desktop in output.txt
+print('phrase searched:' + ' ' + str(key_phrase))
 
+files = glob.glob('/Users/stuart/newcorpus/*.txt')
+# iterate over the list getting each file 
+for file in files:
+   # open the file and then call .read() to get the text 
+   with open(file) as f:
+        text = f.read()
+        tokens = word_tokenize(text)
+        textList = Text(tokens)
+        results = n_concordance_tokenised(textList, key_phrase)
+        phrase_matches = len(results)
+        if phrase_matches > 0:
+            print(str(phrase_matches) + ' ' + 'phrase matches found in' + ' ' + str(file.split('/')[4].split('.')[0]))
 
-# kept adding these and they work dont ask details
-# NOTE MUST install NLTK
+sys.stdout = default_stdout
 
-from __future__ import division
-import nltk, re, pprint, os
-from nltk import sent_tokenize, word_tokenize
-import os
-from nltk.probability import FreqDist
-from collections import Counter
-import operator
-from operator import itemgetter
-import json
-import csv
-from nltk.corpus import stopwords
+print("cleaning up text files...")
 
-stop_words = set(stopwords.words('english'))
+for f in files:
+    os.remove(f)
 
-stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}', '*']) # remove it if you need punctuation 
-
-
-
-# First set the variable path to the directory path.  Use
-# forward slashes (/), even on Windows.  Make sure you
-# leave a trailing / at the end of this variable.
-
-# Path for mac OS X will look something like this:
-path = "/Users/Stuart/newcorpus/"
-
-
-print("Starting")
-
-           
-corpus = []     # Empty corpus to hold corpus documents you put into newcorpus
-emmadict={}     # Empty dictionary to hold word count key
-
-
-# fd=nltk.FreqDist()
-# Iterate through the corpus directory and build the corpus for NLTK.
-listing = os.listdir(path) # start reading the files ino the corpus
-
-for infile in listing:         #Can read single or multiple files saved in PLAIN TEXT-if it doesnt work file saved in wrong format 
-    if infile.startswith('.'): #Mac directories ALWAYS have a .DS_Store file.
-        continue               #This ignores it and other hidden files.
-    url = path + infile        #this section gets the file names from the directory to read in
-    f = open(url);
-    raw = f.read()
-    f.close()
-    tokens = nltk.word_tokenize(raw)    #breaks up the text to single word=tokens
-    tokenmain = [w for w in tokens if not w in stop_words]
-    tokenlower = [w.lower() for w in tokenmain]
-    corpus.append(tokenlower)               #puts the tokens in the corpus-a collection of you processed documents
-    for w in tokenlower:                    #this goes through every word in corpus and counts them up and puts them in a dictionary format key, value pair
-        if w in emmadict:
-            emmadict[w] += 1
-        else:
-            emmadict[w] = 1
-        
-# prints out the results so you know its running                    
-print("Prepared " + str(len(corpus)) + " documents...")
-print("Still Running")
-
-
-#reorders the key, values pair into descending order and store it in results
-
-result = [(v, k) for k, v in emmadict.items()]
-result.sort()
-result.reverse()
-result = [(k, v) for v, k in result]
-
-print(result)
-
-
-#store the key, value pair as comma seperated text in outout.txt
-with open('stopoutput.txt','w') as f:
-    wr = csv.writer(f)
-    wr.writerows(result)
-
-print('Finished')
+print("script finished.")
+print('total time =' + ' ' + str(datetime.now() - startTime))
